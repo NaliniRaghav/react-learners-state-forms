d35d467be2f60f6f52611c667a96601f6a8bdd48
Nalini Raghavendra <naliniraghavendra@nalinis-new-mbp.lan>
2024-09-19 00:15:17 -0400

    forms change

--- src/components/Learner.jsx
+++ src/components/Learner.jsx
@@ -1,22 +1,5 @@
 
-// import Score from "../components/Score.jsx";
-
-// const Learner = ({ learner }) => {
  
-//   return (
-//     <div>
-//        <h2>{learner.name}</h2> 
-//       <p>{learner.bio}</p>
-//       {learner.scores.map((score, index) => (
-//         <Score key={index} score={score} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Learner;
-
-/* eslint-disable react/prop-types */
 import Score from "./Score";
 
 export default function Learner(props) {
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,17 +1,4 @@
-// const Score = ({ score }) => {
-//   console.log(`Rendering score for date: ${score.date}, score: ${score.score}`);
-
-//   return (
-//     <div>
-//       <p>Date: {score.date}</p>
-//       <p>Score: {score.score}</p>
-//     </div>
-//   );
-// };
-
-// export default Score;
-
-/* eslint-disable react/prop-types */
+ 
 export default function Score(props) {
   return (
     <div>
